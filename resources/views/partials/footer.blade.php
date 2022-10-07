f2971c1e55f7d0bc30b528d99f6a60dcf856c628
Francesco Pieraccini <pieraccini.francesco@gmail.com>
2022-10-07 16:11:25 +0200

    add footer header home

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    @include('partials.footer_header')
+    {{-- @include('partials.footer_main')
+    @include('partials.footer_footer') --}}
+  </footer>
---
+++ resources/views/partials/footer_header.blade.php
@@ -0,0 +1,52 @@
+<div class="footer-header">
+    <nav class="container">
+
+        <ul class="footer-list">
+            <li class="footer__list__item">
+                <a href="#nogo">
+                    <img src="{{ asset('./assets/buy-comics-digital-comics.png') }}" alt="">
+                    <span>
+                        digital comics
+                    </span>
+                </a>
+            </li>
+
+            <li class="footer__list__item">
+                <a href="#nogo">
+                    <img src="{{ asset('./assets/buy-comics-merchandise.png') }}" alt="">
+                    <span>
+                        dc merchandise
+                    </span>
+                </a>
+            </li>
+
+            <li class="footer__list__item">
+                <a href="#nogo">
+                    <img src="{{ asset('./assets/buy-comics-subscriptions.png') }}" alt="">
+                    <span>
+                        subscription
+                    </span>
+                </a>
+            </li>
+
+            <li class="footer__list__item">
+                <a href="#nogo">
+                    <img src="{{ asset('./assets/buy-comics-shop-locator.png') }}" alt="">
+                    <span>
+                        comic shop locator
+                    </span>
+                </a>
+            </li>
+
+            <li class="footer__list__item">
+                <a href="#nogo">
+                    <img src="{{ asset('./assets/buy-dc-power-visa.svg') }}" alt="">
+                    <span>
+                        dc power visa
+                    </span>
+                </a>
+            </li>
+        </ul>
+
+    </nav>
+</div>
